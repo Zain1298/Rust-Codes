6a150b7bde474f72b3a1853e3573c5ff1edc271b
Syed Zain Rashid <55804149+Zain1298@users.noreply.github.com>
2020-04-14 20:41:18 +0500

    BInary to Decimal conversion

--- main.rs
+++ main.rs
@@ -1,22 +1,28 @@
 use std::io;
+    
 fn main() {
-    println!("Enter a Decimal number:");
+    println!("Enter a Binary number:");
     let mut input = String::new();
     io::stdin().read_line(&mut input);
     let mut input:i32 = input.trim().parse().unwrap();
 
+    let mut rem = 0;
+    let mut sum = 0;
+    let mut total = 1;
 
-let mut rem = 0;
-let mut sum = 0;
-let mut total = 0;
-let mut i = 1;
+while input != 0{
 
-sum=input;
-while input !=0{
-  rem = input % 2;
-  input = input / 2;
-  total = total + (rem * i);
-  i=i*10;
+rem = input % 10;
+sum = sum + rem*total;
+input = input / 10;
+total = total*2;
+
 }
-println!("Binary Representation of {} is {}",sum,total);
+    println!("Decimal Representation of entered binary number is {}",sum );
+
 }
+
+
+
+
+ 
